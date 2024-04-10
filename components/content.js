bde2cb9f35312d8a536da2e48290363c08a6b328
ben <benvgaming@pm.me>
2024-04-10 21:00:52 +0700

    deleted unused vars

--- components/content.js
+++ components/content.js
@@ -3,25 +3,17 @@ import NextLink from 'next/link'
 import { useLanguage } from './language-context';
 
 import {
-  Link,
-  Container,
   Heading,
   Box,
-  SimpleGrid,
   Button,
-  List,
-  ListItem,
   useColorModeValue,
   chakra
 } from '@chakra-ui/react'
 
-import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
-import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
-import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 
 import Image from 'next/image'
 const translations = {
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,15 @@
-import NextLink from 'next/link'
 import {
   Link,
   Container,
   Heading,
-  Box,
-  SimpleGrid,
   Button,
   List,
   ListItem,
-  useColorModeValue,
-  chakra
 } from '@chakra-ui/react'
-import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
-import Paragraph from '../components/paragraph'
-import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import Image from 'next/image'
 import Content from '../components/content'
-
-const ProfileImage = chakra(Image, {
-  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
-})
 
 const Home = () => (
   <Layout>
