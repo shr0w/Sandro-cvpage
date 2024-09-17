449aefab8f158892cb19687e5a6f100e49a6e2e3
Sandro Vargas García <sandrovargasg@gmail.com>
2024-09-17 22:27:38 +0200

    updates

--- pages/404.js
+++ pages/404.js
@@ -7,20 +7,30 @@ import {
   Divider,
   Button
 } from '@chakra-ui/react'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 
 const NotFound = () => {
+  const { t } = useTranslation('common');
   return (
     <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
+      <Heading as="h1">{t('404.title')}</Heading>
+      <Text>{t('404.text')}</Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <Button as={NextLink} href="/" colorScheme="teal">
-          Return to home
+        {t('404.button')}
         </Button>
       </Box>
     </Container>
   )
 }
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])), // Load the 'common' namespace translations for the current locale.
+    },
+  });
 
 export default NotFound
