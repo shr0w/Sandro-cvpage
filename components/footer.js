84e8c573d7ecd223322a638c1e039ff4d0753703
Sandro Vargas García <sandrovargasg@gmail.com>
2024-09-18 10:44:02 +0200

    update

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => (
   <Box align="center" opacity={0.7} fontSize="sm">
     &copy; {new Date().getFullYear()} Sandro Vargas García. All Rights
-    Reserved. Site based on Takuya Matsuyama&#39s website.
+    Reserved. Site based on Takuya Matsuyama website.
   </Box>
 )
 
